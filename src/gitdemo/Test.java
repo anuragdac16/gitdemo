8e5b4141e19b963309dbca026c8f1c3bf760a0b8
OPERATIVE\anuraggupta <anuraggupta@operative.com>
2018-07-04 17:18:32 +0530

    Adding one println method

--- src/gitdemo/Test.java
+++ src/gitdemo/Test.java
@@ -7,7 +7,9 @@ public class Test {
 	}
 	public static void main(String[] args) {
 		Test test = new Test();
+		System.out.println("Before calling method");
 		test.method();
 		System.out.println("Testing");
+		
 	}
 }
